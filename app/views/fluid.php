0c19f92ab4c3042961cbcb4995d1175c116f576c
bmv437 <bmv437@gmail.com>
2012-06-21 20:23:25 -0400

    fixed baseurl issue needs to be reading from config vars though

--- app/views/fluid.php
+++ app/views/fluid.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
+    <base href="http://brandonvandegrift.com/beta/splice/" />
 
     <!-- Le styles -->
-    <link href="../assets/css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
     <style type="text/css">
       body {
         padding-top: 60px;
@@ -18,7 +19,7 @@
         padding: 9px 0;
       }
     </style>
-    <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
